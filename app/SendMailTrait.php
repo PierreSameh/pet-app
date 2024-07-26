02eee0cab39cd34792b2df23c65f5b08ff6e6fe2
Pierre Sameh <113444132+PierreSameh@users.noreply.github.com>
2024-07-26 18:05:13 +0300

    Update SendMailTrait.php

--- app/SendMailTrait.php
+++ app/SendMailTrait.php
@@ -22,7 +22,7 @@ trait SendMailTrait
             $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
             $mail->SMTPAuth = true; //Enable SMTP authentication
             $mail->Username = 'captainpirzon@gmail.com'; //SMTP username
-            $mail->Password = 'dhpv sgid fdgu zfon'; //SMTP password
+            $mail->Password = ''; //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
             $mail->Port = 465;
             //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
