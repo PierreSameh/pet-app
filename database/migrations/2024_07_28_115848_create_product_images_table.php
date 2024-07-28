303495a81815d98018e4677bd024f269bea8de8a
Pierre Sameh <captainpirzon@gmail.com>
2024-07-28 17:54:32 +0300

    #P Fixing Add Product Images Error

--- database/migrations/2024_07_28_115848_create_product_images_table.php
+++ database/migrations/2024_07_28_115848_create_product_images_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('product_id');
+            $table->integer('product_id');
             $table->string('image');
             $table->string('notes')->nullable();
             $table->timestamps();
 
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            // $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
