d6b2d66b09d56fc34e553e5ef664ec0ab29eb3db
Pierre Sameh <captainpirzon@gmail.com>
2024-08-15 17:44:43 +0300

    #P user details update

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -150,7 +150,7 @@ class AdminController extends Controller
     public function userDetails($userId) {
         $user = User::with(['pets' => function($query) {
             $query->with(['petgallery']);
-        }])->first();
+        }])->find($userId);
         return view('admin.users.details', compact('user'));
     }
 }
